62250f2c8130c5c3d8156a88af3e74427fe55b7e
m7mdtam <mohammedtamimi72@gmail.com>
2024-08-31 10:39:25 +0300

    variables

--- C_Course.c
+++ C_Course.c
@@ -1,11 +1,24 @@
+//frist class : first code
 #include <stdio.h> // main library 
 void main () // main function 
 {
 printf("Hello World\n");
-printf("Hello\t mohammed");
+printf("Hello\t mohammed\n");
 /* for run i have to write 
 gcc fileName.c -o app 
 ./app 
 then it will run 
 */
+
+// second class : variables 
+
+int age  = 25 ; 
+printf(" hello , I'm %i years old",age);
+/*to print the integer value you have to 
+right %i then after the comma write the
+variable name (,age)
+*/
+printf(" %i",age); // print without sentence. 
+
+
 }
