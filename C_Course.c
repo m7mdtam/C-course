d1271b601d9e54b52ec1b4476192bf27c07ed6ac
m7mdtam <mohammedtamimi72@gmail.com>
2024-08-31 10:32:28 +0300

    first commit

---
+++ C_Course.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // main library 
+void main () // main function 
+{
+printf("Hello World\n");
+printf("Hello\t mohammed");
+/* for run i have to write 
+gcc fileName.c -o app 
+./app 
+then it will run 
+*/
+}
